3330de9facfc9492065a212154c29dad7b0e72ae
Olumise <theolumise@gmail.com>
2024-11-25 17:20:34 +0100

    Beginning

---
+++ script.js
@@ -0,0 +1,30 @@
+let serviceEl = document.querySelector("#web-service");
+let imgEl = document.querySelector("#web-img");
+
+// Show the image when mouse enters the service element
+serviceEl.addEventListener("mouseover", function () {
+    imgEl.style.opacity = 1;
+});
+
+// Hide the image when the mouse leaves the service element
+serviceEl.addEventListener("mouseout", function () {
+    imgEl.style.opacity = 0;
+});
+
+// Move the image with the mouse inside the service element
+serviceEl.addEventListener("mousemove", function (event) {
+    handleMouseMove(event);
+});
+
+function handleMouseMove(event) {
+    // Get the bounding box of the parent element
+    let rect = serviceEl.getBoundingClientRect();
+
+    // Calculate the position relative to the parent element
+    let x = event.clientX - rect.left;
+    let y = event.clientY - rect.top;
+
+    // Set the position of the image
+    imgEl.style.left = x + "px";
+    imgEl.style.top = y + "px";
+}
