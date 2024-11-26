8b53d92080c7355d791038e4f2526e8e2e124f1e
Olumise <theolumise@gmail.com>
2024-11-26 22:16:12 +0100

    Added More Sections

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-let serviceEl = document.querySelector("#web-service");
-let imgEl = document.querySelector("#web-img");
 
-// Show the image when mouse enters the service element
-serviceEl.addEventListener("mouseover", function () {
-    imgEl.style.opacity = 1;
-});
 
-// Hide the image when the mouse leaves the service element
-serviceEl.addEventListener("mouseout", function () {
-    imgEl.style.opacity = 0;
-});
+// let serviceEl = document.querySelector("#web-service");
+// let imgEl = document.querySelector("#web-img");
 
-// Move the image with the mouse inside the service element
-serviceEl.addEventListener("mousemove", function (event) {
-    handleMouseMove(event);
-});
+// // Show the image when mouse enters the service element
+// serviceEl.addEventListener("mouseover", function () {
+//     imgEl.style.opacity = 1;
+// });
 
-function handleMouseMove(event) {
-    // Get the bounding box of the parent element
-    let rect = serviceEl.getBoundingClientRect();
+// // Hide the image when the mouse leaves the service element
+// serviceEl.addEventListener("mouseout", function () {
+//     imgEl.style.opacity = 0;
+// });
 
-    // Calculate the position relative to the parent element
-    let x = event.clientX - rect.left;
-    let y = event.clientY - rect.top;
+// // Move the image with the mouse inside the service element
+// serviceEl.addEventListener("mousemove", function (event) {
+//     handleMouseMove(event);
+// });
 
-    // Set the position of the image
-    imgEl.style.left = x + "px";
-    imgEl.style.top = y + "px";
-}
+// function handleMouseMove(event) {
+//     // Get the bounding box of the parent element
+//     let rect = serviceEl.getBoundingClientRect();
+
+//     // Calculate the position relative to the parent element
+//     let x = event.clientX - rect.left;
+//     let y = event.clientY - rect.top;
+
+//     // Set the position of the image
+//     imgEl.style.left = x + "px";
+//     imgEl.style.top = y + "px";
+// }
